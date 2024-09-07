c1c408b8ddd983e23361b0f4691bd56134e17008
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2024-09-07 12:27:44 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { counterReducer } from './store/counter.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterOutputComponent,
+    CounterControlsComponent,
+  ],
+  imports: [BrowserModule, StoreModule.forRoot({counter:counterReducer}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
